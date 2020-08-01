bf59850a0573dba5f6a82ed9c8b5a10db8f96942
Guilherme Rodrigues Simeão <guilhermerodrigues123477@gmail.com>
2020-08-01 10:43:09 -0300

    config project

---
+++ src/database/migrations/1595782768718-CreateAppointments.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateAppointments1595782768718 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {}
+
+    public async down(queryRunner: QueryRunner): Promise<void> {}
+}
